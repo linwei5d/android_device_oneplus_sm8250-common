ec93a0659315916d4c9c5a606aaa90fabdcba7ce
Manster <58349183+linwei5d@users.noreply.github.com>
2023-09-27 21:10:19 +0800

    Update maru_instantnoodlep.mk

--- maru_instantnoodlep.mk
+++ maru_instantnoodlep.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, device/oneplus/instantnoodlep/lineage_instantnoodlep.mk)
+$(call inherit-product, vendor/maruos/device-maru.mk)
 $(call inherit-product, vendor/maruos/BoardConfigVendor.mk)
 
 PRODUCT_NAME := maru_instantnoodlep
@@ -6,3 +7,5 @@ PRODUCT_MODEL := Maru on the instantnoodlep
 
 MARU_VERSION := 0.7-test
 MARU_BUILD_VERSION := $(MARU_VERSION)-$(shell date -u +%Y%m%d)
+DEVICE_PACKAGE_OVERPLAYS += \
+device/oneplus/instantnoodlep/overlay_maru/daydream
